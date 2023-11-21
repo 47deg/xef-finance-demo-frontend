1f1404d6936cf41c3d0076b01d1494621e280ebf
Rafa Paradela <rafa.p@47deg.com>
2023-11-21 12:21:50 -0500

    step two

---
+++ src/components/MrkdwnMsgContent.tsx
@@ -0,0 +1,22 @@
+import { Marked } from '@ts-stack/markdown'
+import { useEffect, useRef } from 'react'
+
+type Props = { content: string; isAssistant?: boolean }
+
+/** For printing content with interleaved JSON codeblocks (denoted with Markdown Syntax). */
+export default function MrkdwnMsgContent({
+                                             content,
+                                             isAssistant = false,
+                                         }: Props) {
+    const parsed = Marked.parse(content)
+    const innerRef = useRef<HTMLDivElement | null>(null)
+
+    useEffect(() => {
+        if (!innerRef.current || !isAssistant) return
+
+        const firstp = innerRef.current.querySelector('p')
+        if (firstp) firstp.innerText = '🤖 ' + firstp.innerText
+    }, [innerRef, isAssistant])
+
+    return <div ref={innerRef} dangerouslySetInnerHTML={{ __html: parsed }}></div>
+}
